47002864a542a21b353f906e594be4b67061e11f
Bas <onrust.bas@gmail.com>
2024-12-08 16:13:43 +0100

    refactor spring web > spring reactive web

--- src/main/java/com/saprone/ingredient/controller/IngredientController.java
+++ src/main/java/com/saprone/ingredient/controller/IngredientController.java
@@ -5,7 +5,7 @@ import com.saprone.ingredient.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class IngredientController {
@@ -18,7 +18,7 @@ public class IngredientController {
     }
 
     @GetMapping("/ingredients")
-    public List<Ingredient> getAllIngredients() {
+    public Flux<Ingredient> getAllIngredients() {
         return ingredientService.getAllIngredients();
     }
 }
--- src/main/java/com/saprone/ingredient/service/IngredientService.java
+++ src/main/java/com/saprone/ingredient/service/IngredientService.java
@@ -1,6 +1,5 @@
 package com.saprone.ingredient.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saprone.ingredient.model.Ingredient;
@@ -9,20 +8,20 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import java.util.List;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @Service
 public class IngredientService {
 
     private final IngredientRepository ingredientRepository;
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
     private static final Logger logger = LoggerFactory.getLogger(IngredientService.class);
     private static final String URL_INGREDIENTS_MEAL_DB = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
 
-    public IngredientService(IngredientRepository ingredientRepository, RestTemplate restTemplate) {
+    public IngredientService(IngredientRepository ingredientRepository, WebClient.Builder webClientBuilder) {
         this.ingredientRepository = ingredientRepository;
-        this.restTemplate = restTemplate;
+        this.webClient = webClientBuilder.build();
     }
 
     @PostConstruct
@@ -31,36 +30,36 @@ public class IngredientService {
             // Check if table ingredients in the database is empty
             if (ingredientRepository.count() == 0) {
                 // Fetching the ingredients from the external API
-                String response = restTemplate.getForObject(URL_INGREDIENTS_MEAL_DB, String.class);
+                JsonNode response = webClient.get()
+                    .uri(URL_INGREDIENTS_MEAL_DB)
+                    .retrieve()
+                    .bodyToMono(JsonNode.class)
+                    .block();
 
-                // Parsing the response and saving each ingredient
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode jsonNode = objectMapper.readTree(response);
-                JsonNode ingredientsNode = jsonNode.path("meals");
+                // Parse the response and save ingredients to the database
+                if (response != null && response.has("meals")) {
+                    for (JsonNode meal : response.get("meals")) {
+                        String ingredientName = meal.get("strIngredient").asText();
 
-                if (ingredientsNode.isArray()) {
-                    for (JsonNode ingredientNode : ingredientsNode) {
-                        String ingredientName = ingredientNode.path("strIngredient").asText();
-
-                        if (!ingredientName.isEmpty()) {
-                            Ingredient ingredient = new Ingredient();
-                            ingredient.setName(ingredientName);
-
-                            ingredientRepository.save(ingredient);
-                        }
+                        // Create and save the ingredient
+                        Ingredient ingredient = new Ingredient();
+                        ingredient.setName(ingredientName);
+                        ingredientRepository.save(ingredient);
                     }
-                }
 
-                logger.info("Ingredients fetched and saved successfully.");
+                    logger.info("Ingredients fetched and saved successfully.");
+                } else {
+                    logger.warn("No ingredients found in the API response.");
+                }
             } else {
                 logger.info("Database is not empty. Skipping fetching and saving ingredients.");
             }
-        } catch (JsonProcessingException e) {
-            logger.error("Error processing JSON response: {}", e.getMessage());
+        } catch (Exception e) {
+            logger.error("Error fetching and saving ingredients: {}", e.getMessage(), e);
         }
     }
 
-    public List<Ingredient> getAllIngredients() {
-        return ingredientRepository.findAll();
+    public Flux<Ingredient> getAllIngredients() {
+        return Flux.fromIterable(ingredientRepository.findAll());
     }
 }
