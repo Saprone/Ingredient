6511352a96e4db32de7c0db75bcd110301492fb7
Bas <onrust.bas@gmail.com>
2024-12-17 00:56:22 +0100

    changed schema

--- src/main/java/com/saprone/ingredient/model/Ingredient.java
+++ src/main/java/com/saprone/ingredient/model/Ingredient.java
@@ -1,20 +1,16 @@
 package com.saprone.ingredient.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
+@Entity
+@Table(name = "ingredient", schema = "ingredient")
 public class Ingredient {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 }
