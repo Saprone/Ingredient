b60fe47414df3b5761bb90269b723e02589c0450
Bas <onrust.bas@gmail.com>
2024-12-17 19:17:33 +0100

    changed name AppConfig

--- src/test/java/com/saprone/ingredient/configuration/AppConfigTest.java
+++ src/test/java/com/saprone/ingredient/configuration/AppConfigTest.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.web.reactive.function.client.WebClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AppConfigurationTest {
+class AppConfigTest {
 
     @Test
     void webClientBuilderBeanShouldExist() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         WebClient.Builder webClientBuilder = context.getBean(WebClient.Builder.class);
         assertThat(webClientBuilder).isNotNull();
     }
