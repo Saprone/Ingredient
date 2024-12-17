761d3052f2543f5a18f7fd0f10ec88a3ec6832d7
Bas <onrust.bas@gmail.com>
2024-12-17 17:37:59 +0100

    Update AppConfigurationTest.java

--- src/test/java/com/saprone/ingredient/configuration/AppConfigurationTest.java
+++ src/test/java/com/saprone/ingredient/configuration/AppConfigurationTest.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.web.reactive.function.client.WebClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppConfigurationTest {
+class AppConfigurationTest {
 
     @Test
-    public void webClientBuilderBeanShouldExist() {
+    void webClientBuilderBeanShouldExist() {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
         WebClient.Builder webClientBuilder = context.getBean(WebClient.Builder.class);
         assertThat(webClientBuilder).isNotNull();
