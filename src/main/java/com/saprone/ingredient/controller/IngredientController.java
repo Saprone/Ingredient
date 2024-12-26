9b68675246e57a2e61d94aa779ce8b6eb0fa5204
Bas <onrust.bas@gmail.com>
2024-12-26 16:11:53 +0100

    Update IngredientController.java

--- src/main/java/com/saprone/ingredient/controller/IngredientController.java
+++ src/main/java/com/saprone/ingredient/controller/IngredientController.java
@@ -5,11 +5,13 @@ import com.saprone.ingredient.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8085")
+@RequestMapping("/ingredients")
 public class IngredientController {
 
     private final IngredientService ingredientService;
@@ -19,7 +21,7 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/ingredients")
+    @GetMapping()
     public Flux<Ingredient> getAllIngredients() {
         return ingredientService.getAllIngredients();
     }
