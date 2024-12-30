bc1754f6e1d21a72e992f6f7716bc7625bed43af
Bas <onrust.bas@gmail.com>
2024-12-30 23:53:06 +0100

    added integration test to pipeline

--- src/test/java/com/saprone/ingredient/controller/IngredientControllerIT.java
+++ src/test/java/com/saprone/ingredient/controller/IngredientControllerIT.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-class IngredientControllerIntegrationTest {
+class IngredientControllerIT {
 
     @LocalServerPort
     private Integer port;
